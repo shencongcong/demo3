c695f236a43e830ce0360c3a8b0a345072fcb541
shencongcong <1343948033@qq.com>
2019-07-08 19:22:37 +0800

    first commit

---
+++ example/test.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: danielshen
+ * Date: 2019/7/8
+ * Time: 19:15
+ */
+
+require(dirname(__DIR__) . '/vendor/autoload.php');
+
+$demo3 = new \Shencongcong\Demo3\Demo3();
+
+var_dump($demo3->test());
---
+++ src/Demo3.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: danielshen
+ * Date: 2019/7/8
+ * Time: 19:13
+ */
+namespace Shencongcong\Demo3;
+
+class Demo3
+{
+    public function test()
+    {
+        return 'this is a demo3';
+    }
+}
